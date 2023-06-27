520a1b1b68cab14d4a4dd3fd8da3b273b2d0a6b9
youssefTarek <youssef3tarek@gmail.com>
2023-06-27 11:56:13 -0700

    first commit

---
+++ To-do-list/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import TodoList from "./components/TodoListFC";
+const App = () => {
+  return (
+    <React.Fragment>
+      <TodoList/>
+    </React.Fragment>
+  );
+};
+
+export default App;
